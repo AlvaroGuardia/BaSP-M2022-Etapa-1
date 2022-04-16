99d43aa9baaaf43e28ad7a5e798230f76c429dbc
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-04-16 19:29:33 -0300

    BaSP-W05-2022:ex01-Variables-and-operators-in-semana-05~

---
+++ semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+// Variable and operators
+
+console.log('-EXERCISE 1: VARIABLES AND OPERATORS');
+
+/*a) Create two numerical variables and use the operator sum for save the value of the sum of both numbers on a 3rd 
+variable.*/
+
+var firstProductPrice = 25;
+var secondProductPrice = 125;
+console.log('The result of exercise 1-a is: ', firstProductPrice + secondProductPrice);
+
+/*b) Create two string type variables and concatenate them saving the result on a 3rd variable. */
+
+var firstName = 'Guardia';
+var lastName = 'Alvaro';
+console.log('The result of exercise 1-b is: ', firstName + lastName);
+
+/*c) Create two string type variables and sum the long of each variable saving the result of the sum on a 3rd variable. */
+
+var partOneName = 'Guardia';
+var partTwoName = 'Alvaro';
+console.log('The result of exercise 1-c is: ', partOneName.length + partTwoName.length);
